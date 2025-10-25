b6333bdc8e89cc473b7defd13de09b31bdb9512f
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2025-10-25 19:30:51 +0530

    s

--- app/management/commands/monthly_gst.py
+++ app/management/commands/monthly_gst.py
@@ -136,13 +136,13 @@ def main():
     )
     invs = pd.DataFrame(qs.iterator())
 
-    qs = models.Inventory.objects.filter(company__group=group, sales__gst_period=period).exclude(txval = 0).values("company_id","bill_id").annotate(
+    qs = models.Inventory.objects.filter(company__group=group, sales__gst_period=period).exclude(txval = 0).values("company_id","bill_id","txval").annotate(
         qty=F('qty') * Func(F('txval'), function='SIGN', output_field=IntegerField()),
         # hsn=F("stock__hsn"),
-        # rt=F('rt') * 2,
-        # cgst=Round(F('rt') * F('txval') / 100,precision=3),
-        # sgst=Round(F('rt') * F('txval') / 100,precision=3),
-    ).values("company_id","bill_id","qty") #,"hsn","rt","cgst","sgst","txval"
+        rt=F('rt') * 2,
+        cgst=Round(F('rt') * F('txval') / 100,precision=3),
+        sgst=Round(F('rt') * F('txval') / 100,precision=3),
+    ).values("company_id","bill_id","qty","rt","cgst","sgst","txval") #,"hsn","rt","cgst","sgst","txval"
     print(invs)
     items = pd.DataFrame(qs.iterator())
     print(items)
