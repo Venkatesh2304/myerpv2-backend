5abc8e200f62f5dce88cdbf82bfabea5d50373f0
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2025-11-08 12:39:02 +0530

    s

--- cron.sh
+++ cron.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -eu
 PROJECT_DIR="/home/ubuntu/myerpv2/backend"
-source $PROJECT_DIR/.venv/bin/activate
+cd $PROJECT_DIR
+source .venv/bin/activate
 systemd-run --scope -p CPUQuota=20% python3 manage.py monthly_gst devaki
