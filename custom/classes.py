6c23e48e9f05c8e0ab1aadc97e55b1ee81c0658c
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2025-12-11 19:14:59 +0530

    s

--- custom/classes.py
+++ custom/classes.py
@@ -964,10 +964,9 @@ class Einvoice(Session) :
         #   logger.info("Response Type : " + res.headers.items() + " Response Code : " + str(res.status_code) )
           logger.info("Content Length : " + str(len(irn_gen_by_me_excel_bytesio)))
           with open("a.xlsx","wb+") as f : f.write(irn_gen_by_me_excel_bytesio)
-          logger.info("Content: ",res.text)
           df = pd.read_excel(BytesIO(irn_gen_by_me_excel_bytesio))
           logger.info(f"Filed Einvoice Dataframe : {df.shape}")
-          logger.info(f"Filed Einvoice Dataframe : {df}")
+          df.to_excel("b.xlsx",index=False)
           return df
           
       #Unverified
